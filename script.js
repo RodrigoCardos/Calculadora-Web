3dcf9c8961776230b70da596d11b08c473edec78
Yasy <rodrigocardos9@gmail.com>
2023-07-07 21:47:26 -0300

    changes in style and button toggle adjusted

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const display = document.querySelector('#display')
 const buttons = document.querySelectorAll('button')
-function changeBg() {
-    const html = document.documentElement
-    html.classList.toggle("light")
-    
-}
 
 buttons.forEach((item) => {
     item.onclick = () => {
@@ -27,9 +22,11 @@ buttons.forEach((item) => {
 const themeToggleBtn = document.querySelector('.theme-toggler')
 const calculator = document.querySelector('.calculator')
 const toggleIcon = document.querySelector('.toggler-icon')
+const html = document.querySelector('html')
 let isDark = true
 themeToggleBtn.onclick = () => {
     calculator.classList.toggle('dark')
     themeToggleBtn.classList.toggle('active')
+    html.classList.toggle('light')
     isDark = !isDark
 }
